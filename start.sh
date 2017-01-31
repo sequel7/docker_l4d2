b3abd36a4a241369bbf9b3153b4b53a4da0286d7
root <root@game-host.maas>
2017-01-31 03:31:58 +0000

    .

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo " "
+echo " "
+
+echo "Make sure you forwarded ports, the command should like something like the below..."
+echo "docker run -it -p 27015:27015 -p 27015:27015/udp sequel7/l4d2"
+
+echo " "
+echo " "
+
 ./steamcmd.sh +login anonymous +force_install_dir /opt/server/ +app_update 222860 +quit
 
 cd /opt/server
